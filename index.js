6abf79d4e1ede51f0a55a1d973ebe511d34aa41e
Suhail Tech info <114290608+SuhailTechInfo@users.noreply.github.com>
2023-06-16 22:03:11 +0500

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const { execFile } = require('child_process');
 const puppeteer = require('puppeteer');
 
 const app = express();
-const PORT = 3000;
+//const PORT = 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
